6a9f611c4b7bed9b9534b50f5b616f647cf76bdc
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-12-20 17:49:31 -0200

    feat: add user addresses model

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 // Executar no terminal: adonis make:controller User (selecionar a opção HTTP)
@@ -7,8 +8,15 @@ class UserController {
   // É recebido o ctx por parâmetro, ele é o contexto da requisição
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password']) // Recuperando SOMENTE estes atributos
+    const addresses = request.input('addresses')
 
-    const user = User.create(data) // Salvando o usuário
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx) // Salvando o usuário
+
+    await user.addresses().createMany(addresses, trx) // Criando todos os endereços recebidos pelo array
+
+    await trx.commit()
 
     return user // Retornando a resposta, não precisa o response, ele já entende automáticamente por conta do --api-only na construção do projeto
   }
