942b3b3b1d830e22aec52053a7ddd5e1fbf05b9f
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-12-20 13:55:49 -0200

    feat: reset user password

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const moment = require('moment')
 const User = use('App/Models/User')
 const Mail = use('Mail')
 
@@ -33,6 +34,33 @@ class ForgotPasswordController {
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Algo não deu certo, este e=mail existe?' }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.only(['token', 'password'])
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'O token de alteração está expirado.' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Algo deu errado ao alterar a sua senha.' }
       })
     }
   }
