f0904ac84f5c6667e562ed403849ec28225b1b98
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-12-20 14:18:14 -0200

    feat: files upload

---
+++ app/Models/File.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// Para gerar o model eu executei: adonis make:model File -m -c
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class File extends Model {
+}
+
+module.exports = File
