0c8558030eb11b4e0c6b3c32ff01c90c4447220b
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-12-20 14:25:37 -0200

    feat: show files path

--- app/Controllers/Http/FileController.js
+++ app/Controllers/Http/FileController.js
@@ -4,6 +4,12 @@ const Helpers = use('Helpers')
 const File = use('App/Models/File')
 
 class FileController {
+  async show ({ params, response }) {
+    const file = await File.findOrFail(params.id)
+
+    return response.download(Helpers.tmpPath(`uploads/${file.file}`))
+  }
+
   async store ({ request, response }) {
     try {
       if (!request.file('file')) return
--- app/Models/File.js
+++ app/Models/File.js
@@ -4,8 +4,17 @@
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
+const Env = use('Env')
 
 class File extends Model {
+  // Adicionando um novo campo no retorno que não está presente no banco
+  static get computed () {
+    return ['url']
+  }
+
+  getUrl ({ id }) {
+    return `${Env.get('APP_URL')}/files/${id}`
+  }
 }
 
 module.exports = File
