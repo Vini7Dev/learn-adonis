5cca8544e813e7933e8d4814587d246d96f577c7
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-12-20 13:33:45 -0200

    feat: send reset password mail

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -14,6 +15,21 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save(user)
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('example@mail.com', 'Example')
+            .subject('Forgot Password')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({
         error: { message: 'Algo não deu certo, este e=mail existe?' }
